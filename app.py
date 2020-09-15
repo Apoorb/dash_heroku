6a8edfc82e931e071a86f63f30ce12a0f6649611
Apoorba Bibeka <apoorb2510@gmail.com>
2020-09-14 22:12:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-app = dash.Dash(__name__)
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 server = app.server
+
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
@@ -13,9 +19,11 @@ app.layout = html.Div([
     ),
     html.Div(id='display-value')
 ])
+
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(value):
     return 'You have selected "{}"'.format(value)
+
 if __name__ == '__main__':
     app.run_server(debug=True)
